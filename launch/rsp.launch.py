5543a8ef4e2ddce104f9e811d915c2bd5a3446af
TheoFCruz <theofonsecacruz@gmail.com>
2025-09-01 02:29:53 +0000

    Gazebo simulation launch

---
+++ launch/rsp.launch.py
@@ -0,0 +1,41 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    pkg_path = get_package_share_directory('vector-field-simul')
+    xacro_file = os.path.join(pkg_path, 'description', 'create3_wrapper.urdf.xacro')
+    robot_description_config = Command([
+        'xacro ', xacro_file,
+        ' sim_mode:=', use_sim_time
+    ])
+
+    # Create a robot_state_publisher node
+    params = {
+        'robot_description': robot_description_config,
+        'use_sim_time': use_sim_time
+    }
+
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    # Launch!
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use sim time if true'),
+        node_robot_state_publisher
+    ])
