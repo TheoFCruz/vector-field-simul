641e923052fa859a7e4494bbbe40563a02e6ebe2
TheoFCruz <theofonsecacruz@gmail.com>
2025-09-04 16:55:50 +0000

    Using irobot_create_control controllers. The robot finally moves

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -4,7 +4,8 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
@@ -73,14 +74,48 @@ def generate_launch_description():
         executable='twist_stamper',
         parameters=[{'use_sim_time': True}],
         remappings=[('/cmd_vel_in','/cmd_vel'),
-                    ('/cmd_vel_out','/diff_drive_controller/cmd_vel')]
+                    ('/cmd_vel_out','/diffdrive_controller/cmd_vel')]
     )
 
+    # Spawner para o Joint State Broadcaster
+    spawn_joint_state_broadcaster = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+        output="screen",
+    )
+
+    # Spawner para o Diff Drive Controller
+    spawn_diff_drive_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diffdrive_controller", "--controller-manager", "/controller_manager"],
+        output="screen",
+    )
+
+    # Garante que os controladores sejam carregados apenas DEPOIS que o robô foi adicionado ao Gazebo.
+    # Quando o nó 'spawn_entity' termina, ele aciona o primeiro spawner.
+    load_joint_state_broadcaster = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_entity,
+            on_exit=[spawn_joint_state_broadcaster],
+        )
+    )
+
+    # Garante que o diff_drive_controller seja carregado depois do joint_state_broadcaster
+    load_diff_drive_controller = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_joint_state_broadcaster,
+            on_exit=[spawn_diff_drive_controller],
+        )
+    )
     return LaunchDescription([
         rsp,
         world_arg,
         gazebo,
         spawn_entity,
         ros_gz_bridge,
-        twist_stamper
+        twist_stamper,
+        load_joint_state_broadcaster,
+        load_diff_drive_controller
     ])
